93b569961e20d5033982fda5ec4cf05bab25ee44
jihun.you <bluegil92@naver.com>
2024-07-03 10:58:33 +0900

    [feature] p37

--- PrimeFactors_KATA/primeFactors.cpp
+++ PrimeFactors_KATA/primeFactors.cpp
@@ -4,9 +4,10 @@ using namespace std;
 class PrimeFactor
 {
 public:
-	vector<int> of(int i)
+	vector<int> of(int number)
 	{
 		vector<int> res = {};
+		if (number == 2) res.push_back(2);
 		return res;
 	}
 };
