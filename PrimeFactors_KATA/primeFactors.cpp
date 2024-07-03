e044875d860fb9565410fdb0abe96d59c14380a5
jihun.you <bluegil92@naver.com>
2024-07-03 11:54:37 +0900

    [refactoring] p59

--- PrimeFactors_KATA/primeFactors.cpp
+++ PrimeFactors_KATA/primeFactors.cpp
@@ -7,23 +7,18 @@ public:
 	vector<int> of(int number)
 	{
 		vector<int> res = {};
+
 		if (number > 1) {
-			int divisor = 2;
-			if (number == 4 || number == 6 || number == 9 || number == 12)
+			for (int divisor = 2; number > 1; divisor++)
 			{
-				for (divisor = 2; number > 1; divisor++)
+				while (number % divisor == 0)
 				{
-					while (number % divisor == 0)
-					{
-						res.push_back(divisor);
-						number /= divisor;
-					}
+					res.push_back(divisor);
+					number /= divisor;
 				}
 			}
-			else {
-				res.push_back(number);
-			}
 		}
+
 		return res;
 	}
 };
