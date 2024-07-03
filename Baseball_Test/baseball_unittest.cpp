c47138c6d7004b8a49c93e46c4326c1a5a18440a
jihun.you <bluegil92@naver.com>
2024-07-03 14:38:17 +0900

    [feature] p27

--- Baseball_Test/baseball_unittest.cpp
+++ Baseball_Test/baseball_unittest.cpp
@@ -1,7 +1,9 @@
 #include "pch.h"
 #include "../Baseball_KATA/baseball.cpp"
+#include <string>
+using namespace std;
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+TEST(BaseballGame, ThrowExceptionWhenInputLengthIsUnmatched) {
+	Baseball game;
+	EXPECT_THROW(game.guess(string("12")), length_error);
 }
